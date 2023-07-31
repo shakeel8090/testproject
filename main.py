6b7be28b3603c3ea4543c3a299fd29b079e738cb
shakeel <shakeel.furaat@gmail.com>
2023-07-31 15:23:27 +0530

    New branch added

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('please like and share the video')
+    name = "try2catch"
+    print_hi(f'please like and share the video:{name}')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
